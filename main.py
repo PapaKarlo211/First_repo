f263c19f806fcff1178f49f56e9c6b8b76e38a59
Bohdan Zahorskiy <papakarlo211@gmail.com>
2025-07-18 14:12:41 +0300

    test

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-print("Hello World")
-print("Hello Git")
+fh = open('test.txt', 'w')
+fh.write('first line\nsecond line\nthird line\nfourth line\nfive line')
+fh.close()
+
+fh = open("test.txt", "r")
+
+position = fh.tell()
+print(position)  # 6
+
+fh.seek(1)
+position = fh.tell()
+print(position)  # 1
+
+fh.read(2)
+position = fh.tell()
+print(position)  # 3
+
+fh.close()
